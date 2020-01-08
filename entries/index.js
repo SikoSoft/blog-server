1fcd108bf56934d4464283d388294809a64fdbec
Aaron Wright <aaron@sikosoft.com>
2020-01-08 16:23:52 +0100

    added entried endpoint

---
+++ entries/index.js
@@ -0,0 +1,8 @@
+const { mockEntries } = require("../util");
+
+module.exports = async function(context, req) {
+  context.res = {
+    status: 200,
+    body: JSON.stringify(mockEntries())
+  };
+};
--- init/index.js
+++ init/index.js
@@ -1,23 +1,23 @@
 const { baseUrl } = require("../util");
 
-module.exports = async function (context, req) {
-    const api = baseUrl(req.originalUrl);
-    console.log("baseUrl", api);
-    context.res = {
-        status: 200,
-        body: JSON.stringify({
-            user: {
-                role: 1
-            },
-            roles: [
-                { id: 0, name: "guest", rights: ["r"] },
-                { id: 1, name: "admin", rights: ["c", "r", "u", "d"] }
-            ],
-            api: {
-                getEntries: `${api}/entries`,
-                getEntriesByTag: `${api}/tags/{tag}`,
-                getTags: `${api}/tags`
-            }
-        })
-    };
+module.exports = async function(context, req) {
+  const api = baseUrl(req.originalUrl);
+  console.log("baseUrl", api);
+  context.res = {
+    status: 200,
+    body: JSON.stringify({
+      user: {
+        role: 1
+      },
+      roles: [
+        { id: 0, name: "guest", rights: ["r"] },
+        { id: 1, name: "admin", rights: ["c", "r", "u", "d"] }
+      ],
+      api: {
+        getEntries: `${api}/entries`,
+        getEntriesByTag: `${api}/tags/{tag}`,
+        getTags: `${api}/tags`
+      }
+    })
+  };
 };
