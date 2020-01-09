b640a35e3fce093f93105b28eb291860cbafc2fc
Aaron Wright <aaron@sikosoft.com>
2020-01-09 21:27:01 +0100

    connect to mariaDB without pool
    fixes a connection handling issue caused by not manually releasing connections

--- database.js
+++ database.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const mariaDB = require("mariadb");
 
-const pool = mariaDB.createPool({
-  host: process.env.DB_HOST,
-  post: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  connectionLimit: 5
-});
-
-module.exports = pool;
+module.exports = {
+  getConnection: async () => {
+    return mariaDB.createConnection({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+    });
+  }
+};
