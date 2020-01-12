bfd4de62c78ee6f8828a9faa572335183a85fe72
Aaron Wright <aaron@sikosoft.com>
2020-01-12 21:13:39 +0100

    get tags
    enables autocomplete through use of starting query

--- tags/index.js
+++ tags/index.js
@@ -1,10 +1,23 @@
-const { mockEntries } = require("../util");
+const { db } = require("../util");
 
 module.exports = async function(context, req) {
-  context.res = {
-    status: 200,
-    body: mockEntries()
-      .map(entry => entry.tags)
-      .reduce((accumulator = [], tags) => [...accumulator, ...tags])
-  };
+  await db.getConnection().then(async connection => {
+    let query = "SELECT * FROM tags";
+    const values = [];
+    if (req.query.tag) {
+      query += " WHERE tag LIKE ?";
+      values.push(`${req.query.tag}%`);
+    }
+    await connection.query(query, values).then(async qRes => {
+      console.log("qRes", qRes);
+      const tags = qRes.map(row => row.tag);
+      context.res = {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: { tags }
+      };
+    });
+  });
 };
