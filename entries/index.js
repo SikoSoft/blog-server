402ee5d7a3084edf57e07e07150ec578a7165b21
Aaron Wright <aaron@sikosoft.com>
2020-01-13 21:52:59 +0100

    entries return tags

--- entries/index.js
+++ entries/index.js
@@ -5,24 +5,39 @@ module.exports = async function(context, req) {
     await connection
       .query("SELECT * FROM entries ORDER BY created DESC")
       .then(async entries => {
-        context.res = {
-          status: 200,
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({
-            entries: entries.map(entry => {
-              const endpoint = `${baseUrl(req.originalUrl)}/entry/${entry.id}`;
-              return {
-                ...entry,
-                api: {
-                  save: { href: endpoint, method: "PUT" },
-                  delete: { href: endpoint, method: "DELETE" }
-                }
-              };
-            })
-          })
-        };
+        let tagQuery = `SELECT * FROM entries_tags WHERE ${[...entries]
+          .fill("entry_id = ?")
+          .join(" || ")}`;
+        await connection
+          .query(
+            tagQuery,
+            entries.map(entry => entry.id)
+          )
+          .then(tagsRes => {
+            context.res = {
+              status: 200,
+              headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify({
+                entries: entries.map(entry => {
+                  const endpoint = `${baseUrl(req.originalUrl)}/entry/${
+                    entry.id
+                  }`;
+                  return {
+                    ...entry,
+                    tags: tagsRes
+                      .filter(tagRow => tagRow.entry_id === entry.id)
+                      .map(tagRow => tagRow.tag),
+                    api: {
+                      save: { href: endpoint, method: "PUT" },
+                      delete: { href: endpoint, method: "DELETE" }
+                    }
+                  };
+                })
+              })
+            };
+          });
       });
   });
 };
