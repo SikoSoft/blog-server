19c1f07d87a352a5c2a81e036c2826967fbcc8a7
Aaron Wright <aaron@sikosoft.com>
2020-04-21 23:47:30 +0200

    entries endpoint returns "end" property
    which specifies whether the end of the entries/drafts has been encountered

--- entries/index.js
+++ entries/index.js
@@ -1,5 +1,15 @@
 const { db, baseUrl, getEndpoint, getSettings } = require("../util");
 
+const getLastEntry = async (connection, query, queryArgs) => {
+  return new Promise((resolve, reject) => {
+    connection
+      .query(`${query} ORDER BY created ASC LIMIT 1`, queryArgs)
+      .then((lastEntry) => {
+        resolve(lastEntry[0].id);
+      });
+  });
+};
+
 const processEntries = (entries, tags, req) => {
   return entries.map((entry) => {
     const endpoint = `${baseUrl(req.originalUrl)}/entry/${entry.id}`;
@@ -47,47 +57,56 @@ const processEntries = (entries, tags, req) => {
 module.exports = async function (context, req) {
   await getSettings().then(async (settings) => {
     await db.getConnection().then(async (connection) => {
-      const q = `SELECT * FROM entries WHERE public = ? ORDER BY created DESC LIMIT ${
-        context.bindingData.start ? parseInt(context.bindingData.start) : 0
-      }, ${settings.per_load ? settings.per_load : 10}`;
-      console.log(q);
-      await connection.query(q, [req.drafts ? 0 : 1]).then(async (entries) => {
-        if (entries.length) {
-          let tagQuery = `SELECT * FROM entries_tags WHERE ${[...entries]
-            .fill("entry_id = ?")
-            .join(" || ")}`;
+      const query = `SELECT * FROM entries WHERE public = ?`;
+      const queryArgs = [req.drafts ? 0 : 1];
+      await getLastEntry(connection, query, queryArgs).then(
+        async (lastEntryId) => {
+          const limit = `LIMIT ${
+            context.bindingData.start ? parseInt(context.bindingData.start) : 0
+          }, ${settings.per_load ? settings.per_load : 10}`;
           await connection
-            .query(
-              tagQuery,
-              entries.map((entry) => entry.id)
-            )
-            .then((tags) => {
-              context.res = {
-                status: 200,
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                  [req.drafts ? "drafts" : "entries"]: processEntries(
-                    entries,
-                    tags,
-                    req
-                  ),
-                }),
-              };
+            .query(`${query} ORDER BY created DESC ${limit}`, queryArgs)
+            .then(async (entries) => {
+              if (entries.length) {
+                let tagQuery = `SELECT * FROM entries_tags WHERE ${[...entries]
+                  .fill("entry_id = ?")
+                  .join(" || ")}`;
+                await connection
+                  .query(
+                    tagQuery,
+                    entries.map((entry) => entry.id)
+                  )
+                  .then((tags) => {
+                    context.res = {
+                      status: 200,
+                      headers: {
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify({
+                        [req.drafts ? "drafts" : "entries"]: processEntries(
+                          entries,
+                          tags,
+                          req
+                        ),
+                        end: entries[entries.length - 1].id === lastEntryId,
+                      }),
+                    };
+                  });
+              } else {
+                context.res = {
+                  status: 200,
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    [req.drafts ? "drafts" : "entries"]: [],
+                    end: true,
+                  }),
+                };
+              }
             });
-        } else {
-          context.res = {
-            status: 200,
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              [req.drafts ? "drafts" : "entries"]: [],
-            }),
-          };
         }
-      });
+      );
     });
   });
 };
