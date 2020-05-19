4617f4048f9f6e3f1a3bad9c21276c8f6f61a268
Aaron Wright <aaron@sikosoft.com>
2020-05-19 20:28:03 +0200

    save the database connection for performance
    See #19

--- database.js
+++ database.js
@@ -1,14 +1,19 @@
 require("dotenv").config();
 const mariaDB = require("mariadb");
 
+let connection = false;
+
 module.exports = {
   getConnection: async () => {
-    return mariaDB.createConnection({
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME
-    });
-  }
+    if (!connection) {
+      connection = mariaDB.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+      });
+    }
+    return connection;
+  },
 };
