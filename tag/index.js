05db1787372e678fc88793abcfe8113e04bbc28b
Aaron Wright <aaron@sikosoft.com>
2020-06-08 17:53:07 +0200

    tag endpoint implements pagination
    Fix #30

--- tag/index.js
+++ tag/index.js
@@ -1,40 +1,60 @@
-const { db, jsonReply, processEntry } = require("../util.js");
+const {
+  db,
+  jsonReply,
+  processEntry,
+  getSettings,
+  getLastEntry,
+} = require("../util.js");
 
 module.exports = async function (context, req) {
-  await db.getConnection().then(async (connection) => {
-    await connection
-      .query("SELECT entry_id FROM entries_tags WHERE tag = ?", [
-        context.bindingData.tag,
-      ])
-      .then(async (tagEntries) => {
-        const entriesWithTag = tagEntries.map((tagEntry) => tagEntry.entry_id);
-
-        console.log("tagEntries", tagEntries);
-        await connection
-          .query(
-            `SELECT * FROM entries_tags WHERE ${[...tagEntries]
-              .fill("entry_id = ?")
-              .join(" || ")}`,
-            entriesWithTag
-          )
-          .then(async (entriesTags) => {
-            console.log("entriesWithTag", entriesWithTag);
-            await connection
-              .query(
-                `SELECT * FROM entries WHERE (${[...entriesWithTag]
-                  .fill("id = ?")
-                  .join(" || ")}) && public = 1 ORDER BY created DESC`,
-                entriesWithTag
-              )
-              .then((entries) => {
-                console.log("entries", entries);
-                jsonReply(context, {
-                  entries: entries.map((entry) =>
-                    processEntry(req, entry, entriesTags)
-                  ),
-                });
-              });
-          });
-      });
+  await getSettings().then(async (settings) => {
+    await db.getConnection().then(async (connection) => {
+      await connection
+        .query("SELECT entry_id FROM entries_tags WHERE tag = ?", [
+          context.bindingData.tag,
+        ])
+        .then(async (tagEntries) => {
+          const entriesWithTag = tagEntries.map(
+            (tagEntry) => tagEntry.entry_id
+          );
+          await connection
+            .query(
+              `SELECT * FROM entries_tags WHERE ${[...tagEntries]
+                .fill("entry_id = ?")
+                .join(" || ")}`,
+              entriesWithTag
+            )
+            .then(async (entriesTags) => {
+              const query = `SELECT * FROM entries WHERE (${[...entriesWithTag]
+                .fill("id = ?")
+                .join(" || ")}) && public = 1`;
+              await getLastEntry(query, entriesWithTag).then(
+                async (lastEntryId) => {
+                  const limit = `LIMIT ${
+                    context.bindingData.start
+                      ? parseInt(context.bindingData.start)
+                      : 0
+                  }, ${settings.per_load ? settings.per_load : 10}`;
+                  await connection
+                    .query(
+                      `${query} ORDER BY created DESC ${limit}`,
+                      entriesWithTag
+                    )
+                    .then((entries) => {
+                      jsonReply(context, {
+                        entries: entries.map((entry) =>
+                          processEntry(req, entry, entriesTags)
+                        ),
+                        end:
+                          entries.length === 0
+                            ? true
+                            : entries[entries.length - 1].id === lastEntryId,
+                      });
+                    });
+                }
+              );
+            });
+        });
+    });
   });
 };
