5cc2555458b00367f3812521b4e1c0e3a2bbadca
Aaron Wright <aaron@sikosoft.com>
2020-06-20 00:05:48 +0200

    fixes issue with saveFilterOrder

--- saveFilterOrder/index.js
+++ saveFilterOrder/index.js
@@ -5,11 +5,12 @@ module.exports = async function (context, req) {
     typeof req.body === "string" ? parse(req.body) : req.body ? req.body : {};
   await db.getConnection().then(async (connection) => {
     if (body.orderedFilters) {
-      body.orderedFilers.forEach((filter, index) => {
+      await body.orderedFilters.forEach(async (filter, index) => {
         connection
-          .query("UPDATE filters SET order = ? WHERE id = ?", [index, filter])
+          .query("UPDATE filters SET `order` = ? WHERE id = ?", [index, filter])
           .then(async () => {});
       });
+      jsonReply(context, { success: true });
     } else {
       context.res = {
         status: 400,
