2905272586616df244c5890434ab1fb9629dd632
Aaron Wright <aaron@sikosoft.com>
2021-10-19 20:59:26 +0200

    regex linting fix

--- util.js
+++ util.js
@@ -38,7 +38,7 @@ function sanitizeTitle(title) {
   return title
     .toLowerCase()
     .replace(/ /g, "-")
-    .replace(/[^a-z0-9\-]/g, "");
+    .replace(/[^a-z0-9-]/g, "");
 }
 
 async function getSettings() {
