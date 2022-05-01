fed36f5bb15d9ea0fd5c965d7b230b92a299da0c
Aaron Wright <aaron@sikosoft.com>
2022-05-01 19:50:28 +0200

    Fix #37 setup script now imports default rights & settings

--- db.sql
+++ db.sql
@@ -1,17 +1,25 @@
 #
 # SQL Export
 # Created by Querious (302003)
-# Created: 30 April 2022 at 20:43:52 CEST
+# Created: 1 May 2022 at 19:46:20 CEST
 # Encoding: Unicode (UTF-8)
 #
+
+
 SET @ORIG_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS;
 SET FOREIGN_KEY_CHECKS = 0;
+
 SET @ORIG_UNIQUE_CHECKS = @@UNIQUE_CHECKS;
 SET UNIQUE_CHECKS = 0;
+
 SET @ORIG_TIME_ZONE = @@TIME_ZONE;
 SET TIME_ZONE = '+00:00';
+
 SET @ORIG_SQL_MODE = @@SQL_MODE;
 SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO';
+
+
+
 CREATE TABLE `comments` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `entry_id` varchar(255) NOT NULL,
@@ -20,39 +28,51 @@ CREATE TABLE `comments` (
   `time` int(10) DEFAULT 0,
   `public` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 53 DEFAULT CHARSET = utf8 AVG_ROW_LENGTH = 1820;
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb3 AVG_ROW_LENGTH=1820;
+
+
 CREATE TABLE `comments_scores` (
   `comment_id` int(10) NOT NULL DEFAULT 0,
   `score` float DEFAULT 0,
   PRIMARY KEY (`comment_id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8 AVG_ROW_LENGTH = 780;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 AVG_ROW_LENGTH=780;
+
+
 CREATE TABLE `entries` (
-  `id` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `title` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `body` longtext CHARACTER SET utf8 DEFAULT NULL,
+  `id` varchar(255) CHARACTER SET utf8mb3 NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb3 NOT NULL,
+  `body` longtext CHARACTER SET utf8mb3 DEFAULT NULL,
   `created` int(11) DEFAULT 0,
   `last_edited` int(11) DEFAULT 0,
   `public` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+
 CREATE TABLE `entries_tags` (
   `entry_id` varchar(255) NOT NULL DEFAULT '0',
   `tag` varchar(64) NOT NULL DEFAULT '0',
-  UNIQUE KEY `entry_tag` (`entry_id`, `tag`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+  UNIQUE KEY `entry_tag` (`entry_id`,`tag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
 CREATE TABLE `filters` (
-  `id` varchar(64),
-  `label` varchar(255),
-  `image` varchar(255),
+  `id` varchar(64) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
   `order` int(3) DEFAULT 0,
   `enabled` tinyint(1) DEFAULT 1
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
 CREATE TABLE `filters_rules` (
-  `filter_id` varchar(64),
-  `type` varchar(32),
-  `value` varchar(255),
-  `operator` varchar(16)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+  `filter_id` varchar(64) DEFAULT NULL,
+  `type` varchar(32) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  `operator` varchar(16) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
 CREATE TABLE `roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
@@ -62,57 +82,76 @@ CREATE TABLE `roles` (
   `d` tinyint(1) DEFAULT 0,
   `token` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 3 DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+
+
 CREATE TABLE `roles_rights` (
   `role` int(6) DEFAULT NULL,
   `action` varchar(64) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8 AVG_ROW_LENGTH = 2048;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 AVG_ROW_LENGTH=2048;
+
+
 CREATE TABLE `settings` (
-  `role_admin` int(6) DEFAULT 0,
-  `role_guest` int(6) DEFAULT 0,
-  `toast_life` int(10) DEFAULT 0,
-  `enable_comments` tinyint(1) DEFAULT 0,
-  `use_captcha` tinyint(1) DEFAULT 0,
-  `min_score_auto_publish` float DEFAULT 1,
-  `max_failed_token_attempts` int(11) DEFAULT 0,
-  `failed_token_lockout_duration` int(11) DEFAULT 0,
-  `github_feed` varchar(255) NOT NULL,
-  `show_sidebar` tinyint(1) DEFAULT 0,
-  `per_load` tinyint(3) DEFAULT 0,
-  `teaser_mode` tinyint(1) DEFAULT 0,
-  `auto_entry_url` tinyint(1) DEFAULT 0
-) ENGINE = InnoDB DEFAULT CHARSET = utf8 AVG_ROW_LENGTH = 16384;
+  `id` varchar(255) NOT NULL,
+  `int` int(10) DEFAULT 0,
+  `float` float DEFAULT 0,
+  `varchar` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
 CREATE TABLE `tags` (
   `tag` varchar(64) NOT NULL,
   `weight` float NOT NULL DEFAULT 0,
   PRIMARY KEY (`tag`),
   UNIQUE KEY `tag_UNIQUE` (`tag`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
 CREATE TABLE `tags_rights` (
   `tag` varchar(64) DEFAULT NULL,
   `role` int(6) DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
 CREATE TABLE `tokens` (
-  `token` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `token` varchar(64) CHARACTER SET utf8mb3 NOT NULL,
   `one_time` tinyint(1) DEFAULT 0,
   `consumed` int(6) DEFAULT 0,
   `role` int(3) DEFAULT 0,
   PRIMARY KEY (`token`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+
 CREATE TABLE `tokens_consumed` (
   `token` varchar(64) NOT NULL,
   `ip` varchar(15) NOT NULL,
   `time` int(10) DEFAULT 0,
   `session` varchar(255) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
 CREATE TABLE `tokens_invalid_attempts` (
   `token` varchar(64) NOT NULL,
   `ip` varchar(15) NOT NULL,
   `time` int(10) DEFAULT 0
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+
+
+
+
 SET FOREIGN_KEY_CHECKS = @ORIG_FOREIGN_KEY_CHECKS;
+
 SET UNIQUE_CHECKS = @ORIG_UNIQUE_CHECKS;
+
 SET @ORIG_TIME_ZONE = @@TIME_ZONE;
 SET TIME_ZONE = @ORIG_TIME_ZONE;
+
 SET SQL_MODE = @ORIG_SQL_MODE;
-# Export Finished: 30 April 2022 at 20:43:52 CEST
+
+
+
+# Export Finished: 1 May 2022 at 19:46:20 CEST
+
--- setup.js
+++ setup.js
@@ -1,18 +1,69 @@
-const { db } = require('./util.js');
-const fs = require('fs');
-const path = require('path');
+const { db } = require("./util.js");
+const fs = require("fs");
+const path = require("path");
+const { rights, settings, typeMap } = require("blog-spec");
+const { exit } = require("process");
 
-const setup = async () => {
-  const dbCon = await db.getConnection();
-  const queries = fs
-    .readFileSync(path.join(__dirname, 'db.sql'), { encoding: 'UTF-8' })
-    .split(';\n');
-  for (query of queries) {
-    if (query.length && !query.match(/\/\*/)) {
-      await dbCon.query(query);
+let dbCon;
+let errors = [];
+
+const setupRights = async () => {
+  dbCon = await db.getConnection();
+  const query = "INSERT INTO roles_rights (role, action) VALUES(?, ?)";
+  for (action of Object.keys(rights)) {
+    for (role of rights[action].defaultRoles) {
+      dbCon.query(query, [role, action]).catch((error) => {
+        errors.push(error);
+      });
     }
   }
-  await dbCon.end();
+};
+
+const setupSettings = async () => {
+  dbCon = await db.getConnection();
+  const query =
+    "INSERT INTO settings (`id`, `int`, `float`, `varchar`) VALUES(?, ?, ?, ?)";
+  for (id of Object.keys(settings)) {
+    const type = settings[id].dataType
+      ? settings[id].dataType
+      : typeMap[settings[id].type];
+    const value = settings[id].default
+      ? settings[id].default
+      : type === "varchar"
+      ? ""
+      : 0;
+    await dbCon
+      .query(query, [
+        id,
+        type === "int" ? value : 0,
+        type === "float" ? value : 0,
+        type === "varchar" ? value : "",
+      ])
+      .catch((error) => {
+        errors.push(error);
+      });
+  }
+};
+
+const setupTables = async () => {
+  dbCon = await db.getConnection();
+  const queries = fs
+    .readFileSync(path.join(__dirname, "db.sql"), { encoding: "UTF-8" })
+    .split(";\n");
+  for (query of queries) {
+    if (query.length && !query.match(/\/\*/)) {
+      await dbCon.query(query).catch((error) => {
+        errors.push(error);
+      });
+    }
+  }
+};
+
+const setup = async () => {
+  await setupTables();
+  await setupRights();
+  await setupSettings();
+  return;
 };
 
 setup();
