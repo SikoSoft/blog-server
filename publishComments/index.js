291e34d2fd166bb06047abaecfab4ad2190a4584
Aaron Wright <aaron@sikosoft.com>
2022-05-03 20:55:50 +0200

    more escape from async hell

--- publishComments/index.js
+++ publishComments/index.js
@@ -1,19 +1,15 @@
 const { db, jsonReply } = require("../util");
 
-module.exports = async function(context, req) {
+module.exports = async function (context, req) {
   const body =
     typeof req.body === "string" ? parse(req.body) : req.body ? req.body : {};
   const { ids } = body;
-  await db.getConnection().then(async connection => {
-    await connection
-      .query(
-        `UPDATE comments SET public = 1 WHERE ${[...ids]
-          .fill("id = ?")
-          .join(" || ")}`,
-        ids
-      )
-      .then(async qRes => {
-        jsonReply(context, { numPublished: qRes.affectedRows });
-      });
-  });
+  const connection = await db.getConnection();
+  const qRes = await connection.query(
+    `UPDATE comments SET public = 1 WHERE ${[...ids]
+      .fill("id = ?")
+      .join(" || ")}`,
+    ids
+  );
+  jsonReply(context, { numPublished: qRes.affectedRows });
 };
--- tagRoles/index.js
+++ tagRoles/index.js
@@ -1,11 +1,9 @@
 const { db, jsonReply } = require("../util.js");
 
 module.exports = async function (context) {
-    await db.getConnection().then(async (connection) => {
-        await connection
-          .query("SELECT * FROM tags_rights ORDER by `tag`, `role`")
-          .then(async (rights) => {
-            jsonReply(context, rights);
-          });
-      });
-}
+  const connection = await db.getConnection();
+  const rights = await connection.query(
+    "SELECT * FROM tags_rights ORDER by `tag`, `role`"
+  );
+  jsonReply(context, rights);
+};
