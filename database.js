57ffe9060b87229681e603905c383efc301d15c9
Aaron Wright <aaron@sikosoft.com>
2022-05-11 21:17:24 +0200

    added knex for abstracting SQL querys away from a vendor like mariadb

--- database.js
+++ database.js
@@ -1,18 +1,22 @@
 require("dotenv").config();
+const knex = require("knex");
 const mariaDB = require("mariadb");
 
 let connection = false;
+let connectionNew = false;
+
+const connectionObject = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+};
 
 module.exports = {
   getConnection: async () => {
     if (!connection) {
-      connection = mariaDB.createConnection({
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-      });
+      connection = mariaDB.createConnection(connectionObject);
       connection.then((openConnection) => {
         openConnection.on("error", (error) => {
           console.log("Database connection error", error);
@@ -21,5 +25,17 @@ module.exports = {
       });
     }
     return connection;
+  },
+
+  getConnectionNew: async () => {
+    try {
+      connectionNew = knex({
+        client: "mysql",
+        connection: connectionObject,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+    return connectionNew;
   },
 };
