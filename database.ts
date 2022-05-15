70858fcfb5a59a3167b55b871cb2322ee0188184
Aaron Wright <aaron@sikosoft.com>
2022-05-15 21:42:56 +0200

    cleaned up database.ts from old obsolete code

--- database.ts
+++ database.ts
@@ -1,23 +1,22 @@
 import "dotenv/config";
-import { Knex, knex }  from "knex";
+import { Knex, knex } from "knex";
 
 let connection: Promise<Knex>;
 
 const connectionObject = {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 };
 
 export async function getConnection(): Promise<Knex<any, any[]>> {
-  return connection || knex({
-    client: "mysql",
-    connection: connectionObject
-  })
-}
-
-export default {
-  getConnection
+  return (
+    connection ||
+    knex({
+      client: "mysql",
+      connection: connectionObject,
+    })
+  );
 }
