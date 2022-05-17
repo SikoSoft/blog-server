8710f2a32322af17311c0247a1f8555cd22619b6
Aaron Wright <aaron@sikosoft.com>
2022-05-17 17:07:30 +0200

    removed console log statements from entries

--- entries/index.ts
+++ entries/index.ts
@@ -18,20 +18,17 @@ const httpTrigger: AzureFunction = async function (
   const settings = await getSettings();
   const connection = await getConnection();
   const excludedEntries = await getExcludedEntries(req.headers["sess-token"]);
-  //const drafts = req.headers.type && req.headers.type === "draft";
   const query = connection
     .select("*")
     .from("entries")
     .whereNotIn("id", excludedEntries)
     .andWhere({ listed: 1, public: drafts ? 0 : 1 });
-  console.log("FIRST", query.toSQL().toNative());
   const lastEntryId = await getLastEntry(query.clone());
   const rawEntries = await query
     .clone()
     .orderBy("created", "desc")
     .offset(context.bindingData.start ? parseInt(context.bindingData.start) : 0)
     .limit(settings.per_load ? settings.per_load : 10);
-  console.log("SECOND", query.toSQL().toNative());
   if (rawEntries.length) {
     const processedEntries = rawEntries.map((entry) =>
       processEntry(req, entry)
